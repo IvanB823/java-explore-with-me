13fd15707b14d05f215fc1cea13e59a9a1a11b74
Ivan Baturo <baturo.ivan@bk.ru>
2025-10-11 18:39:15 +0300

    feat: evmstatservice added

---
+++ evm-stats-service/src/main/java/ru/practicum/evmstatsservice/EvmStatsServiceApplication.java
@@ -0,0 +1,15 @@
+package ru.practicum.evmstatsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.practicum.statsclient.StatsClientImpl;
+
+@SpringBootApplication
+@Import({StatsClientImpl.class})
+public class EwmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwmServiceApplication.class, args);
+    }
+}
