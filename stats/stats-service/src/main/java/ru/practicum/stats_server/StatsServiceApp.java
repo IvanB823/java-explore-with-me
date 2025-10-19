b61ebb5857c41edbb9c907c2ccce5d870655fd66
Ivan Baturo <baturo.ivan@bk.ru>
2025-10-19 22:00:33 +0300

    feat: stats-service classes updated

--- stats/stats-service/src/main/java/ru/practicum/stats_server/StatsServiceApp.java
+++ stats/stats-service/src/main/java/ru/practicum/stats_server/StatsServiceApp.java
@@ -1,13 +1,13 @@
-package ru.practicum.statsservice;
+package ru.practicum.stats_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatsServiceApplication {
+public class StatsServiceApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(StatsServiceApplication.class, args);
+        SpringApplication.run(StatsServiceApp.class, args);
     }
 
 }
